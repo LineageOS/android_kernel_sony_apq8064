e8a45afd96dba1a2247800a8909485dd194afbdd
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-05-15 19:32:23 +0200

    Merge of Sony release 10.1.1.A.1.253
    Add support for Xperia Tab Z
     * pollux            - SGP321
     * pollux_windy      - SGP311/2
     * pollux_dcm        - SO-03E
     * gaga_kddi         - unknown
    
    CAF:
     * Revert "msm: mdp: Initialize vsync sysfs in probe"
     * msm_fb: display: Create sysfs entries in mdp_probe for MDP4 targets
     * Update copyright to The Linux Foundation
     * ARM: decompressor: avoid speculative prefetch from non-RAM areas
     * power: pm8921-bms: split batterydata interpolation into a new file
     * power: pm8921-bms: use configurable poll times
     * msm: event_timer: Fix potential race condition
     * msm: mdm: Assert soft_reset_gpio in mdm_power_down_common
     * msm: Ensure krait pll clamp is always set for CPU0
     * msm: platsmp: Update Krait reset sequence
     * msm: spm_devices: Fix failure to set vdd during cpu_up
     * msm: wcnss: Dump PMIC regulator contol registers
     * Bluetooth: Added SSR state machine in hci_smd sriver
     * diag: Improve handling of IOCTLs
     * msm: vidc: Adds AU delimiter support for AVC
     * msm: wfd: Clear out stale flags for output buffers
     * msm: vidc: Adds VUI timing info support for AVC encoding
     * msm: vidc: Set CODECCONFIG for buffers with sps/pps
     * msm: wfd: Avoid exiting mdp task thread on error
     * ASoC: wcd9xxx: Fix MBHC irq handler deadlock scenario
     * net: usb: Fix memory leak on Tx data path
     * usbnet: decrease suspend count if returning -EBUSY for runtime suspend
     * wcnss: Log CCPU registers in reset interrupt API
     * USB: ci13xxx_udc: Don't flush and disable all endpoints during disconnect
     * USB: ci13xxx_udc: Avoid flushing endpoint twice during disable
     * USB: ci13xxx_udc: Don't flush inactive endpoints
     * USB: ci13xxx_udc: Skip flushing endpoints upon failure
     * EHCI: HSIC: Handle asynchronous interrupt properly
     * EHCI: HSIC: Disable asynchronous interrupt when wakeup gpio is present
     * usb: ks_bridge: Avoid allocating large buffers in atomic context
     * usb: ks_bridge: Limit write size to 16KB
     * usb: mdm_ctrl_bridge: Do not unlink URB upon cable disconnect
     * USB: msm_otg: Add 50 msec delay after changing scope from SYSTEM to DEVICE
    
    Change-Id: Ibe3a6f61bd270c76cbfa904110a7d0e6d935916f

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -1,5 +1,5 @@
-/* Copyright (c) 2008-2012, Code Aurora Forum. All rights reserved.
- * Copyright (C) 2012 Sony Mobile Communications AB.
+/* Copyright (c) 2008-2012, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2012-2013 Sony Mobile Communications AB.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -35,6 +35,7 @@
 #include "mipi_dsi.h"
 #include "mdp.h"
 #include "mdp4.h"
+#include "mipi_dsi_panel_driver.h"
 
 u32 dsi_irq;
 u32 esc_byte_ratio;
@@ -583,6 +584,16 @@ static int mipi_dsi_probe(struct platform_device *pdev)
 		goto mipi_dsi_probe_err;
 
 	pdev_list[pdev_list_cnt++] = pdev;
+
+	if (mfd->panel_pdev) {
+		struct mipi_dsi_data *dsi_data;
+		dsi_data = platform_get_drvdata(mfd->panel_pdev);
+		if (!dsi_data)
+			return -ENODEV;
+		if (mipi_dsi_pdata && mipi_dsi_pdata->dsi_power_save)
+			dsi_data->dsi_power_save =
+				mipi_dsi_pdata->dsi_power_save;
+	}
 
 return 0;
 
