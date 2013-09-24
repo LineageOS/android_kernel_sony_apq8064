bebc7b27ac743f0524a646daa44a2a1747ada72b
Laura Abbott <lauraa@codeaurora.org>
2013-09-24 10:41:26 -0700

    cma: Add support for memory limits
    Currently, when dynamically placing regions CMA will allow the memory
    to be placed anywhere, including highmem. Due to system restrictions,
    regions may need to be placed in a smaller range. Add support to
    devicetree to allow these regions to have an upper bound on where they
    will be placed.
    
    Change-Id: Ib4ae194cbb6389e1091e7e04cfd331e9ab67ad05
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    [skhiani@codeaurora.org: Resolve merge conflicts]
    Signed-off-by: Susheel Khiani <skhiani@codeaurora.org>

--- drivers/base/dma-contiguous.c
+++ drivers/base/dma-contiguous.c
@@ -226,6 +226,7 @@ int __init cma_fdt_scan(unsigned long node, const char *uname,
 	__be32 *prop;
 	char *name;
 	bool in_system;
+	phys_addr_t limit = MEMBLOCK_ALLOC_ANYWHERE;
 
 	if (!of_get_flat_dt_prop(node, "linux,contiguous-region", NULL))
 		return 0;
@@ -241,9 +242,13 @@ int __init cma_fdt_scan(unsigned long node, const char *uname,
 	in_system =
 		of_get_flat_dt_prop(node, "linux,reserve-region", NULL) ? 0 : 1;
 
-	pr_info("Found %s, memory base %lx, size %ld MiB\n", uname,
-		(unsigned long)base, (unsigned long)size / SZ_1M);
-	dma_contiguous_reserve_area(size, &base, MEMBLOCK_ALLOC_ANYWHERE, name,
+	prop = of_get_flat_dt_prop(node, "linux,memory-limit", NULL);
+	if (prop)
+		limit = be32_to_cpu(prop[0]);
+
+	pr_info("Found %s, memory base %lx, size %ld MiB, limit %pa\n", uname,
+		(unsigned long)base, (unsigned long)size / SZ_1M, &limit);
+	dma_contiguous_reserve_area(size, &base, limit, name,
 					in_system);
 
 	return 0;
