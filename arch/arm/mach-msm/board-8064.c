5dc35bb75112b00a741495f98e868783a9ed6eed
olivieer <yves.olli@gmail.com>
2014-08-08 13:33:05 -0400

    Implement kexec-hardboot
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel
    previously loaded in memory by kexec.  This works around the problem of
    soft-booted kernel hangs due to improper device shutdown and/or
    reinitialization."
    More info in /arch/arm/Kconfig.
    
    Original author: Mike Kasick <mike@kasick.org>
    
    Vojtech Bocek <vbocek@gmail.com>:
      I've ported it to mako and flo, it is based of my grouper port, which is
      based of Asus TF201 patche ported by Jens Andersen <jens.andersen@gmail.com>
    
      I've moved atags copying from guest to the host kernel, which means there
      is no need to patch the guest kernel, assuming the --mem-min in kexec call
      is within the first 256MB of System RAM, otherwise it will take a long time
      to load. I've also fixed /proc/atags entry, which would give the kexec-tools
      userspace binary only the first 1024 bytes of atags,
      see arch/arm/kernel/atags.c for more details.
    
      Other than that, memory-reservation code for the hardboot page and
      some assembler to do the watchdog reset on MSM chip are new for this device.
    
    ayysir <dresadd09691@gmail.com>:
      kexec: use mem_text_write_kernel_word to set reboot_code_buffer args
      in order to avoid protection faults (writes to read-only
      kernel memory) when CONFIG_STRICT_MEMORY_RWX is enabled.
    
    xylon-reborn <xylon-reborn@outlook.com>:
      Fix crash notes compiling error
    
    Change-Id: Ib8184dd9a76e5d567b61ed3de8f014318c5da7ab
    Signed-off-by: Vojtech Bocek <vbocek@gmail.com>
    Signed-off-by: olivieer <yves.olli@gmail.com>

--- arch/arm/mach-msm/board-8064.c
+++ arch/arm/mach-msm/board-8064.c
@@ -791,6 +791,17 @@ static void __init apq8064_reserve(void)
 {
 	apq8064_set_display_params(prim_panel_name, ext_panel_name,
 		ext_resolution);
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page, just before the ram_console
+	//struct membank* bank = &meminfo.bank[0];
+	//phys_addr_t start = bank->start + bank->size - SZ_1M - 0x00300000;
+	phys_addr_t start = KEXEC_HB_PAGE_ADDR;
+	int ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
 	msm_reserve();
 }
 
