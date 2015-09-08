13b876f8a6f72fa9aa4681464b16f70df8ceaaa8
Binoy Jayan <bjayan@codeaurora.org>
2015-09-08 13:58:00 +0530

    msm: Fixes for static analysis issues - set 1
    Resolved the following issues reported by
    static analysis scan on mach-msm.
    
    arch/arm/mach-msm/board-8064.c +671 | reserve_ion_memory():
    Pointer 'pdata' checked before dereference.
    
    arch/arm/mach-msm/board-8960.c +772 | reserve_ion_memory():
    Pointer 'pdata' checked before dereference.
    
    arch/arm/mach-msm/dma.c +344 | msm_dmov_enqueue_cmd_ext_work():
    Pointer 'cmd' checked before dereference.
    
    arch/arm/mach-msm/iommu_domains.c +186, +209 |
                                      msm_iommu_map_contig_buffer():
    Return value from fn 'msm_get_iommu_domain' checked for NULL
    before dereference.
    
    arch/arm/mach-msm/mdm_common.c +693 | mdm_panic_prep():
    Pointer 'mdm_drv' checked before dereference.
    
    arch/arm/mach-msm/msm-buspm-dev.c +154 | msm_buspm_dev_ioctl():
    Pointer 'dbgbuf' checked before dereference.
    
    arch/arm/mach-msm/peripheral-loader.c +174 | load_segment():
                                          +269 | load_image():
    Pointer 'fw' checked before dereference.
    
    arch/arm/mach-msm/pmu.c +60 | multicore_free_irq():
    Pointer 'desc' checked before dereference.
    
    arch/arm/mach-msm/qdsp6v2/audio_acdb.c +237 | get_hw_delay():
    Pointer 'entry' checked before dereference.
    
    arch/arm/mach-msm/qdsp6v2/audio_utils_aio.c +456
                                         | audio_aio_unmap_ion_region():
    Pointer 'region' checked before dereference.
    
    arch/arm/mach-msm/spm-v2.c +369 | void msm_spm_drv_set_avs_vlevel():
    Pointer 'dev' checked before dereference.
    
    arch/arm/mach-msm/wcnss-ssr-8960.c +126 | riva_post_bootup():
    Pointer 'pwlanconfig' checked before dereference.
    
    drivers/char/diag/diag_dci.c +508 | diag_process_dci_transaction():
    Pointer 'temp' not used when NULL.
    
    drivers/input/touchscreen/atmel_mxt_ts.c +2083 | mxt_load_fw():
    Pointer 'fw' checked before dereference.
    
    Conflicts:
    	arch/arm/mach-msm/board-8064.c
    	drivers/media/video/msm_wfd/enc-mfc-subdev.c
    	arch/arm/mach-msm/iommu_domains.c
    	arch/arm/mach-msm/mdm_common.c
    	arch/arm/mach-msm/msm-buspm-dev.c
    
    Change-Id: I469a64427fe84cb99e10121eb76c4429013cfaef
    Signed-off-by: Jaiju Yesudasan <cjaijuy@codeaurora.org>
    Signed-off-by: Binoy Jayan <bjayan@codeaurora.org>

--- arch/arm/mach-msm/qdsp6v2/audio_acdb.c
+++ arch/arm/mach-msm/qdsp6v2/audio_acdb.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -208,7 +208,7 @@ int get_hw_delay(int32_t path, struct hw_delay_entry *entry)
 		pr_err("ACDB=> %s Invalid path: %d\n",__func__,path);
 
 	if ((delay == NULL) || ((delay != NULL) && delay->num_entries == 0)) {
-		pr_debug("ACDB=> %s Invalid delay/ delay entries\n", __func__);
+		pr_err("ACDB=> %s Invalid delay/ delay entries\n", __func__);
 		result = -EINVAL;
 		goto done;
 	}
@@ -235,11 +235,11 @@ done:
 	mutex_unlock(&acdb_data.acdb_mutex);
 ret:
 	if (entry)
-		pr_debug("ACDB=>%s: Path=%d samplerate=%u usec=%u status %d\n",
+		pr_err("ACDB=>%s: Path=%d samplerate=%u usec=%u status %d\n",
 			__func__, path, entry->sample_rate,
 			entry->delay_usec, result);
 	else
-		pr_debug("ACDB=> %s: Path=%d entry=%p status %d\n",
+		pr_err("ACDB=> %s: Path=%d entry=%p status %d\n",
 			__func__, path, entry, result);
 	return result;
 }
