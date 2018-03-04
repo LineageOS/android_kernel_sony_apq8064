94304d8e1907d1494f4477dff4012dce3df0c930
palaych <palaychm@yandex.ru>
2018-03-04 22:22:24 +0300

    fs: Workaround the compiler's bad optimization
    When compiling the kernel with  the compiler
    "aarch64-linux-android-gcc (GCC) 4.9.x-google 20140827 (prerelease)"
    The compiler seems to be optimizing a value out in the do_sync_write
    function and is incorrectly passing the pointer instead of the
    dereferenced value.
    Force the compiler to use the right value by passing the dereferenced
    pointer instead of another variable that is assigned with the dereferenced
    value.
    
    Change-Id: I60505ffe39393f6323dcc7c6f912c74ea6aca6cd
    Signed-off-by: Nikhilesh Reddy <reddyn@codeaurora.org>

--- fs/read_write.c
+++ fs/read_write.c
@@ -403,7 +403,7 @@ ssize_t do_sync_write(struct file *filp, const char __user *buf, size_t len, lof
 	kiocb.ki_nbytes = len;
 
 	for (;;) {
-		ret = filp->f_op->aio_write(&kiocb, &iov, 1, kiocb.ki_pos);
+		ret = filp->f_op->aio_write(&kiocb, &iov, 1, *ppos);
 		if (ret != -EIOCBRETRY)
 			break;
 		wait_on_retry_sync_kiocb(&kiocb);
